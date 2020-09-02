8c05f929da31d4acb819d6ced6dcc6d8e41c8053
kimdonguke <rlaehddnr100@gmail.com>
2020-09-02 21:55:54 +0900

    first commit

--- app/src/main/java/com/example/suapp/MainActivity.java
+++ app/src/main/java/com/example/suapp/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.suapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button name, hak;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name=findViewById(R.id.name);
+        hak=findViewById(R.id.hak);
+        name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "김동욱", Toast.LENGTH_SHORT).show();
+            }
+        });
+        hak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "20501", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
